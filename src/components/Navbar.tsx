3cc640615e0760a70822f596dfcc6f020c79a732
syd <yifan.d.chen@gmail.com>
2024-12-10 06:46:46 -0500

    edit navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,13 @@
-import Link from "next/link"
-import { Icons } from "./Icons"
-import { buttonVariants } from "./ui/Button"
-import SearchBar from "./SearchBar"
+import { authOptions } from '@/lib/auth'
+import { getServerSession } from 'next-auth'
+import Link from 'next/link'
+import { Icons } from './Icons'
+import { buttonVariants } from './ui/Button'
+import { UserAccountNav } from './UserAccountNav'
+import SearchBar from './SearchBar'
 
-const Navbar = () => {
-
+const Navbar = async () => {
+  const session = await getServerSession(authOptions)
   return <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z=[10] py-2">
     <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
       {/* logo */}
@@ -16,10 +19,16 @@ const Navbar = () => {
       {/* search bar */}
       <SearchBar />
 
-      <Link href="/sign-in" className={buttonVariants()}>Sign In</Link>
-
+        {/* actions */}
+        {session?.user ? (
+          <UserAccountNav user={session.user} />
+        ) : (
+          <Link href='/sign-in' className={buttonVariants()}>
+            Sign In
+          </Link>
+        )}
+      </div>
     </div>
-  </div>
 }
 
 export default Navbar
